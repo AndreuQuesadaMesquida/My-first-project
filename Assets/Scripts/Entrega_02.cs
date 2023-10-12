0c8d2cbce808463e6d308e96a2db6d88414ae36c
AndreuQuesadaMesquida <andreuquesadamesquida@gmail.com>
2023-10-12 09:49:29 +0200

    tasca avance

--- Assets/Scripts/Entrega_02.cs
+++ Assets/Scripts/Entrega_02.cs
@@ -14,11 +14,16 @@ public class Entrega_02 : MonoBehaviour
 
     // Exercise 4:
 
-    // Pendiente
+    // pendiente
 
     // Exercise 5:
 
     [SerializeField] private string username = "Andreu";
+
+    // Exercise 7:
+
+    [SerializeField] private int var1;
+    [SerializeField] private int var2;
 
     // Exercise 2:
 
@@ -41,9 +46,29 @@ public class Entrega_02 : MonoBehaviour
 
         Debug.Log($"Welcome {username}");
 
+        // Exercise 8:
+
+        Debug.Log($"{var1} % {var2} = {var1 % var2}");
+
     }
 
+    // Exercise 9:
 
+    // pendiente
+
+    //Exercise 10:
+
+    /*
+    Exercise 1: The creation of the private variables num1 with number 2 and num2 with number 3 assigned to them in the inspector
+    Exercise 2: The result displayed in the cosnole by the sum of the two variables is 5
+    Exercise 3: The first variable created (num1) isn't greater than or equal to the second one (num 2) but instead is lower
+    Exercise 4: 
+    Exercise 5: The creation of a private string named "Andreu"
+    Exercise 6: The message "Welcome Andreu" is displayed in the console
+    Exercise 7: The creation of the private variables var1 with number 14 and var2 with number 6 assigned to them in the inspector
+    Exercise 8: The result of the variable modulo displayed in the console is 2     ¿?*
+    Exercise 9: 
+    */
 
 
 
